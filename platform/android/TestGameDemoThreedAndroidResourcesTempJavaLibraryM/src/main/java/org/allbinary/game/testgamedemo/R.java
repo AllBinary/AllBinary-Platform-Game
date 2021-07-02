1f42c9e9dd1ce9b5111c902885752349dd4edbf9
Travis Berthelot <travisberthelot@allbinary.com>
2021-07-01 22:35:44 -0500

    Update temp R file

--- platform/android/TestGameDemoThreedAndroidResourcesTempJavaLibraryM/src/main/java/org/allbinary/game/testgamedemo/R.java
+++ platform/android/TestGameDemoThreedAndroidResourcesTempJavaLibraryM/src/main/java/org/allbinary/game/testgamedemo/R.java
@@ -11,58 +11,56 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int testgamedemo_icon=0x7f020000;
-        public static final int testgamedemo_wait_256_by_256=0x7f020001;
+        public static final int testgamedemo_icon=0x7f020011;
+        public static final int testgamedemo_wait_256_by_256=0x7f020012;
     }
     public static final class id {
         public static final int ad_area_bottom=0x7f060002;
         public static final int ad_area_left=0x7f060003;
         public static final int ad_area_right=0x7f060004;
         public static final int ad_area_top=0x7f060001;
+        public static final int root=0x7f060005;
         public static final int testgamedemo=0x7f060000;
-        public static final int testgamedemo_gl=0x7f060005;
+        public static final int testgamedemo_gl=0x7f060007;
     }
     public static final class layout {
         public static final int testgamedemo_ad_overlay_layout=0x7f030000;
         public static final int testgamedemo_gl_ad_overlay_layout=0x7f030001;
         public static final int testgamedemo_gl_layout=0x7f030002;
         public static final int testgamedemo_layout=0x7f030003;
-        public static final int testgamedemo_min3d_ad_overlay_layout=0x7f030003;
-        public static final int testgamedemo_min3d_layout=0x7f030004;        
+        public static final int testgamedemo_min3d_ad_overlay_layout=0x7f030004;
+        public static final int testgamedemo_min3d_layout=0x7f030005;
     }
     public static final class raw {
-    	public static final int error=0x7f040000;
-        public static final int licenseinitdata=0x7f040001;
-        public static final int partner=0x7f040002;
-        public static final int select=0x7f040003;
-        public static final int test=0x7f040004;
-        public static final int testgamedemo_wait_256_by_256=0x7f040005;
-        public static final int touch_button_128_by_128=0x7f040026;
-        public static final int touch_button_64_by_64=0x7f040027;
-        public static final int touch_button_down_arrow_128_by_128=0x7f04002a;
-        public static final int touch_button_down_arrow_64_by_64=0x7f04002b;
-        public static final int touch_button_generic_action_128_by_128=0x7f04002c;
-        public static final int touch_button_generic_action_64_by_64=0x7f04002d;
-        public static final int touch_button_start_128_by_128=0x7f040038;
-        public static final int touch_button_start_64_by_64=0x7f040039;
-        public static final int touch_button_strafe_left_arrow_128_by_128=0x7f04003a;
-        public static final int touch_button_strafe_left_arrow_64_by_64=0x7f04003b;
-        public static final int touch_button_strafe_right_arrow_128_by_128=0x7f04003c;
-        public static final int touch_button_strafe_right_arrow_64_by_64=0x7f04003d;
-        public static final int touch_button_turn_left_arrow_128_by_128=0x7f04003e;
-        public static final int touch_button_turn_left_arrow_64_by_64=0x7f04003f;
-        public static final int touch_button_turn_right_arrow_128_by_128=0x7f040040;
-        public static final int touch_button_turn_right_arrow_64_by_64=0x7f040041;
-        public static final int touch_button_up_arrow_128_by_128=0x7f040042;
-        public static final int touch_button_up_arrow_64_by_64=0x7f040043;
-        
-        public static final int ouya_a=0x7f040012;
-        public static final int ouya_key=0x7f040013;
-        public static final int ouya_o=0x7f040014;
-        public static final int ouya_u=0x7f040015;
-        public static final int ouya_y=0x7f040016;   
-        
-        public static final int threed_obj=0x7f040044;
+        public static final int licenseinitdata=0x7f040008;
+        public static final int locked_demo_game_feature_128_by_128=0x7f040009;
+        public static final int locked_demo_game_feature_64_by_64=0x7f04000a;
+        public static final int ouya_a=0x7f04000d;
+        public static final int ouya_o=0x7f04000e;
+        public static final int ouya_u=0x7f04000f;
+        public static final int ouya_y=0x7f040010;
+        public static final int partner=0x7f040011;
+        public static final int select=0x7f040013;
+        public static final int test=0x7f040014;
+        public static final int testgamedemo_wait_256_by_256=0x7f040015;
+        public static final int touch_button_128_by_128=0x7f040017;
+        public static final int touch_button_64_by_64=0x7f040018;
+        public static final int touch_button_down_arrow_128_by_128=0x7f040019;
+        public static final int touch_button_down_arrow_64_by_64=0x7f04001a;
+        public static final int touch_button_generic_action_128_by_128=0x7f04001b;
+        public static final int touch_button_generic_action_64_by_64=0x7f04001c;
+        public static final int touch_button_start_128_by_128=0x7f04001d;
+        public static final int touch_button_start_64_by_64=0x7f04001e;
+        public static final int touch_button_strafe_left_arrow_128_by_128=0x7f04001f;
+        public static final int touch_button_strafe_left_arrow_64_by_64=0x7f040020;
+        public static final int touch_button_strafe_right_arrow_128_by_128=0x7f040021;
+        public static final int touch_button_strafe_right_arrow_64_by_64=0x7f040022;
+        public static final int touch_button_turn_left_arrow_128_by_128=0x7f040023;
+        public static final int touch_button_turn_left_arrow_64_by_64=0x7f040024;
+        public static final int touch_button_turn_right_arrow_128_by_128=0x7f040025;
+        public static final int touch_button_turn_right_arrow_64_by_64=0x7f040026;
+        public static final int touch_button_up_arrow_128_by_128=0x7f040027;
+        public static final int touch_button_up_arrow_64_by_64=0x7f040028;
     }
     public static final class string {
         public static final int progressbar_in_title_string=0x7f050001;
