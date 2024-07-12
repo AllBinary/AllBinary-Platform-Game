42f033e757e2d77bb2fe323818b92a8cb3bd5e39
Travis Berthelot <travisberthelot@allbinary.com>
2024-07-12 17:59:47 -0500

    Add AndroidResources class.

---
+++ platform/android/TestGameDemoThreedAndroidGradleM/src/main/other/org/allbinary/AndroidResources.java
@@ -0,0 +1,99 @@
+package org.allbinary;
+
+import org.allbinary.game.testgamedemo.R;
+
+public class AndroidResources
+{
+    private static final org.allbinary.AndroidResources instance = new org.allbinary.AndroidResources();
+
+    /**
+     * @return the instance
+     */
+    public static org.allbinary.AndroidResources getInstance()
+    {
+        return instance;
+    }
+
+    public class Rdrawable {
+        public final int testgamedemo_icon = R.drawable.testgamedemo_icon;
+        public final int testgamedemo_wait_256_by_256 = R.drawable.testgamedemo_wait_256_by_256;
+    }
+
+    public org.allbinary.AndroidResources.Rdrawable drawable = new org.allbinary.AndroidResources.Rdrawable();
+
+    public class Rid {
+        public final int custom_web_view = R.id.custom_web_view;
+        public final int testgamedemo = R.id.testgamedemo;
+        public final int testgamedemo_gl = R.id.testgamedemo_gl;
+        public final int ad_area_bottom = R.id.ad_area_bottom;
+        public final int ad_area_left = R.id.ad_area_left;
+        public final int ad_area_right = R.id.ad_area_right;
+        public final int ad_area_top = R.id.ad_area_top;
+    }
+
+    public org.allbinary.AndroidResources.Rid id = new org.allbinary.AndroidResources.Rid();
+
+    public class Rlayout {
+        public final int webview_layout = R.layout.webview_layout;
+        public final int testgamedemo_ad_overlay_layout = R.layout.testgamedemo_ad_overlay_layout;
+        public final int testgamedemo_gl_layout = R.layout.testgamedemo_gl_layout;
+        public final int testgamedemo_layout = R.layout.testgamedemo_layout;
+        public final int testgamedemo_min3d_layout = R.layout.testgamedemo_min3d_layout;
+        public final int testgamedemo_gl_ad_overlay_layout = R.layout.testgamedemo_gl_ad_overlay_layout;
+        public final int testgamedemo_min3d_ad_overlay_layout = R.layout.testgamedemo_min3d_ad_overlay_layout;
+        
+    }
+
+    public org.allbinary.AndroidResources.Rlayout layout = new org.allbinary.AndroidResources.Rlayout();
+
+    public class Rraw {
+        public final int threed_obj = R.raw.threed_obj;
+        public final int about = R.raw.about;
+        public final int privacy_policy = R.raw.privacy_policy;
+        public final int begin = R.raw.begin;
+        public final int error = R.raw.error;
+        public final int explosion = R.raw.explosion;
+        public final int test = R.raw.test;
+        public final int licenseinitdata = R.raw.licenseinitdata;
+        public final int locked_demo_game_feature_64_by_64 = R.raw.locked_demo_game_feature_64_by_64;
+        public final int locked_demo_game_feature_128_by_128 = R.raw.locked_demo_game_feature_128_by_128;
+        public final int ouya_a = R.raw.ouya_a;
+        //public final int ouya_key = R.raw.ouya_key;
+        public final int ouya_o = R.raw.ouya_o;
+        public final int ouya_u = R.raw.ouya_u;
+        public final int ouya_y = R.raw.ouya_y;
+        public final int partner = R.raw.partner;
+        public final int select = R.raw.select;
+        public final int touch_button_64_by_64 = R.raw.touch_button_64_by_64;
+        public final int touch_button_128_by_128 = R.raw.touch_button_128_by_128;
+        public final int touch_button_down_arrow_64_by_64 = R.raw.touch_button_down_arrow_64_by_64;
+        public final int touch_button_down_arrow_128_by_128 = R.raw.touch_button_down_arrow_128_by_128;
+        public final int touch_button_generic_action_64_by_64 = R.raw.touch_button_generic_action_64_by_64;
+        public final int touch_button_generic_action_128_by_128 = R.raw.touch_button_generic_action_128_by_128;
+        public final int touch_button_start_64_by_64 = R.raw.touch_button_start_64_by_64;
+        public final int touch_button_start_128_by_128 = R.raw.touch_button_start_128_by_128;
+        public final int touch_button_strafe_left_arrow_64_by_64 = R.raw.touch_button_strafe_left_arrow_64_by_64;
+        public final int touch_button_strafe_left_arrow_128_by_128 = R.raw.touch_button_strafe_left_arrow_128_by_128;
+        public final int touch_button_strafe_right_arrow_64_by_64 = R.raw.touch_button_strafe_right_arrow_64_by_64;
+        public final int touch_button_strafe_right_arrow_128_by_128 = R.raw.touch_button_strafe_right_arrow_128_by_128;
+        public final int touch_button_turn_left_arrow_64_by_64 = R.raw.touch_button_turn_left_arrow_64_by_64;
+        public final int touch_button_turn_left_arrow_128_by_128 = R.raw.touch_button_turn_left_arrow_128_by_128;
+        public final int touch_button_turn_right_arrow_64_by_64 = R.raw.touch_button_turn_right_arrow_64_by_64;
+        public final int touch_button_turn_right_arrow_128_by_128 = R.raw.touch_button_turn_right_arrow_128_by_128;
+        public final int touch_button_up_arrow_64_by_64 = R.raw.touch_button_up_arrow_64_by_64;
+        public final int touch_button_up_arrow_128_by_128 = R.raw.touch_button_up_arrow_128_by_128;
+        public final int youlose = R.raw.youlose;
+        public final int youwin = R.raw.youwin;
+    }
+
+    public org.allbinary.AndroidResources.Rraw raw = new org.allbinary.AndroidResources.Rraw();
+
+//    public class Rstring {
+//        public final int grey_stripe_id = R.string.grey_stripe_id;
+//        public final int minispacewar_vector_options_app_name = R.string.minispacewar_vector_options_app_name;
+//        public final int progressbar_text = R.string.progressbar_text;
+//        public final int vdopia_id = R.string.vdopia_id;
+//    }
+//
+//    public Rstring string = new Rstring();
+}
